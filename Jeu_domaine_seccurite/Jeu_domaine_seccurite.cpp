45d35abebeee4650fe8b8b3396928a92cbe10f22
Fafishyle <finaritralft@gmail.com>
2024-03-11 16:29:06 +0100

    Creation du jeu
    Affichage Terminal
    Les commandes sont non fonctionnelles

---
+++ Jeu_domaine_seccurite/Jeu_domaine_seccurite.cpp
@@ -0,0 +1,28 @@
+// Jeu_domaine_seccurite.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name_user = "";
+    cout << "Bienvenue dans le jeu de la securite!\n Insère ton nom: \n";
+    cin >> name_user;
+    cout << "Bonjour "<< name_user << "! \n";
+    cout << "On a trouvé ce disque dur"<<
+        "On sait qu'il contient une clé de porte monnaie de crypto"<<
+        "Il faut explorer le filesystem et trouver des indices pour trouver la clé!";
+    cout << "Tu vas entrer dans un simulateur de terminal d'une machine." <<
+        "Tu vas découvrir comment marche la sécurité dans les réseaux en général.\n";
+    cout << "______________________Entrée________________________\n";
+
+    string enter_user = "";
+    while (enter_user != "Exit" || enter_user != "exit")
+    {
+        cout << "C:/Users/"<<name_user<<":~$ ";
+        cin >> enter_user;
+        cout << "\n";
+    }
+    
+}
