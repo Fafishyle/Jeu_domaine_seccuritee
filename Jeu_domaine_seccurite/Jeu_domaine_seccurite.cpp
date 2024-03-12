c807307ba096d25812044dfb6f190f0086dce5a4
Fafishyle <finaritralft@gmail.com>
2024-03-12 10:50:39 +0100

    Correction de la boucle while et de l'affichage de la sconsigne

--- Jeu_domaine_seccurite/Jeu_domaine_seccurite.cpp
+++ Jeu_domaine_seccurite/Jeu_domaine_seccurite.cpp
@@ -10,22 +10,22 @@ using namespace std;
 int main()
 {
     string name_user = "";
-    cout << "Bienvenue dans le jeu de la securite!\n Insère ton nom: \n";
+    cout << "Bienvenue dans le jeu de la securite!\n Insere ton nom: \n";
     cin >> name_user;
     cout << "Bonjour "<< name_user << "! \n";
-    cout << "On a trouvé ce disque dur"<<
-        "On sait qu'il contient une clé de porte monnaie de crypto"<<
-        "Il faut explorer le filesystem et trouver des indices pour trouver la clé!";
-    cout << "Tu vas entrer dans un simulateur de terminal d'une machine." <<
-        "Tu vas découvrir comment marche la sécurité dans les réseaux en général.\n";
-    cout << "______________________Entrée________________________\n";
+    cout << "On a trouve ce disque dur"<<
+        "On sait qu'il contient une cle de porte monnaie de crypto.\n"<<
+        "Il faut explorer le filesystem et trouver des indices pour trouver la clef! \n"<<
+        "Tu vas entrer dans un simulateur de terminal d'une machine. \n " <<
+        "Tu vas decouvrir comment marche la securite.\n";
+    cout << "______________________Entree________________________\n";
 
     string enter_user = "";
-    while (enter_user != "Exit" || enter_user != "exit")
+    while (enter_user != "Exit" && enter_user != "exit")
     {
         cout << "C:/Users/"<<name_user<<":~$ ";
         cin >> enter_user;
-        cout << "Votre réponse : "<<enter_user<<"\n";
+        // cout << "Votre réponse : "<<enter_user<<"\n";
     }
     cout << "Jeu fini ! \n";
     return 0;
